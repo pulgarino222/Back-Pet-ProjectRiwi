4dda203f5363a0a39324575d2967e876e914bfb1
coder-emanuel <emanu6a@gmail.com>
2024-09-11 17:38:35 -0500

    feat:connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Hacer que ConfigModule esté disponible en toda la aplicación
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10), // toma el valor del puerto para la base de datos desde el entorno, lo convierte en un número entero y lo asigna a la propiedad port.
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // No recomendado en producción, pero útil en desarrollo
+    }),
+  ],
 })
 export class AppModule {}
