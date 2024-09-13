0c98b64e825af40ab6702c2e5dae34cf90633e25
pulgarino222 <pulgarinhernandezjuanfelipe@gmail.com>
2024-09-13 17:02:30 -0500

    feat:we add jwt and barrel files already for up to develop

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/modules/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { jwtConfig } from 'src/config/configurationsJwt';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [jwtConfig],
+      isGlobal: true, // Opcional: si quieres que la configuración esté disponible globalmente
+    }),
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('secret'),
+        signOptions: configService.get('signOptions'),
+      }),
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
--- src/modules/config.module.ts
+++ src/modules/config.module.ts
@@ -1,14 +1,16 @@
 // config/config.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule, ConfigService } from '@nestjs/config';
-import config from '../config/configurations';
+import {ConfigDataBase,jwtConfig } from '../config/barrelconfig'
 
 @Module({
   imports: [
     NestConfigModule.forRoot({
-      load: [config],
+      load: [ConfigDataBase,jwtConfig],
       isGlobal: true, // Si deseas que la configuración esté disponible en toda la aplicación
     }),
+
+    
   ],
   providers: [ConfigService],
   exports: [ConfigService],
