dc46a2b41a121bc328e10941c48888bb75a17449
pulgarino222 <pulgarinhernandezjuanfelipe@gmail.com>
2024-09-14 12:53:56 -0500

    Descripción de los cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users.module';
+import { UsersController } from './controller/users.controller';
+import { UsersService } from './services/users.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
