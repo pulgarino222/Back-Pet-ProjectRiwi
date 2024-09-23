7b725ae47324f2315e5f593db72f7e28627604b7
sotomen10 <soto.alexis@hotmail.com>
2024-09-22 21:44:32 -0500

    Update synchronization settings to use .env; add comments for clarity.

--- src/modules/pets.module.ts
+++ src/modules/pets.module.ts
@@ -8,9 +8,19 @@ import { PetSpecies } from 'src/entities/petSpecies.entity';
 import { PetsService } from 'src/services/pets.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Pet,PetMedia,PetSpecies])],
-    providers:[PetsService,CloudinaryService],
-    exports:[PetsService,CloudinaryService],
-    controllers:[PetsController]
+    imports: [
+        TypeOrmModule.forFeature([Pet, PetMedia, PetSpecies]) // Import the TypeORM module for the specified entities
+    ],
+    providers: [
+        PetsService, // Service for pet-related operations
+        CloudinaryService // Service for handling Cloudinary operations
+    ],
+    exports: [
+        PetsService, // Export PetsService for use in other modules
+        CloudinaryService // Export CloudinaryService for use in other modules
+    ],
+    controllers: [
+        PetsController // Controller for handling pet-related requests
+    ]
 })
 export class PetsModule {}
